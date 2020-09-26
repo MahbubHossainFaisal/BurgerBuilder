c23b1f68370e4f0d24ab72c42bcfb727b47603ed
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2020-09-27 01:43:08 +0600

    Connecting state with build control (more)

--- app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,21 +3,47 @@ import Dux from '../../hoc/Dux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    bacon: 0.4,
+    cheese: 1.3,
+    meat: 0.7
+};
 class BurgerBuilder extends Component {
 
+
+  
     state ={
         ingredients: {
             salad: 0,
             bacon:0,
             cheese: 0,
             meat:0,
-        }
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) =>{
+        const oldCount=this.state.ingredients[type];
+        const updatedCount=oldCount+1;
+        const updatedIngredients={
+            ...this.state.ingredients
+        };
+        updatedIngredients[type]=updatedCount;
+        const oldPrice = this.state.totalPrice;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const newPrice = oldPrice+priceAddition;
+        
+        this.setState({ingredients:updatedIngredients, totalPrice:newPrice});
+
     }
     render() {
         return (
            <Dux>
                <Burger ingredients={this.state.ingredients} />
-               <BuildControls />
+               <BuildControls 
+               ingredientAdded={this.addIngredientHandler} />
            </Dux>
         );
     }
