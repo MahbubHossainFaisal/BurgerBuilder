e9b5285df0e6fadfb7c1327c750d31e6edf451f4
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2020-10-02 01:19:37 +0600

    adding reusable navigation items

--- app/src/components/Navigation/Toolbar/Toolbar.js
+++ app/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Toolbar.css';
 import Sign from '../../Sign/Sign';
+import NavigationItems from '../NavigationItems/NavigationItems';
 const Toolbar = (props) => (
 
     <header className="Toolbar">
@@ -8,7 +9,7 @@ const Toolbar = (props) => (
 
        <Sign />
         <nav>
-           ...
+           <NavigationItems />
         </nav>
     </header>
 );
