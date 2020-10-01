d9e3e704a62067b57fc36c193dbc06dcf73576c3
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2020-10-02 02:43:33 +0600

    Creating Responsive SideDrawer

--- app/src/components/Layout/Layout.js
+++ app/src/components/Layout/Layout.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Dux from '../../hoc/Dux';
 import './Layout.css';
-import Toolbar from '../Navigation/Toolbar/Toolbar'
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
 
   <Dux>
+   
         <Toolbar />
+    <SideDrawer />
         <main className='Layout-content'>
             {props.children}
         </main>
--- app/src/components/Navigation/Toolbar/Toolbar.js
+++ app/src/components/Navigation/Toolbar/Toolbar.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './Toolbar.css';
 import Sign from '../../Sign/Sign';
 import NavigationItems from '../NavigationItems/NavigationItems';
+
 const Toolbar = (props) => (
 
     <header className="Toolbar">
         <div>Menu</div>
 
-       <Sign />
+       <Sign height="80%" />
         <nav>
            <NavigationItems />
         </nav>
