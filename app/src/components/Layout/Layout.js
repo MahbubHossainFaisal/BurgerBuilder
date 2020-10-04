e6bb96b2a680d416bd944dff3ce490cf70fb4467
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2020-10-04 14:40:42 +0600

    Adding toggler button for side drawer

--- app/src/components/Layout/Layout.js
+++ app/src/components/Layout/Layout.js
@@ -13,11 +13,17 @@ class Layout extends Component{
   sideDrawerClosedHandler = () =>{
     this.setState({showSideDrawer:false});
   }
+
+  sideDrawerToggleHandler = () =>{
+    this.setState((prevState)=>{
+      return {showSideDrawer : !prevState.showSideDrawer};
+    });
+  }
   
      render(){
        return(
       <Dux>
-        <Toolbar />
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
         <SideDrawer open={this.state.showSideDrawer} closed ={this.sideDrawerClosedHandler} />
         <main className='Layout-content'>
             {this.props.children}
---
+++ app/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DrawerToggle = (props) =>(
+    <div onClick={props.clicked} style={{color: 'white'}}>
+        Menu
+    </div>
+);
+
+export default DrawerToggle;
